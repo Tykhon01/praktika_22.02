10b6414a1d168b1f1b0d37bf2cc13e7dccc882e1
Tykhon Shevchuk <tykhon000shevchuk@gmail.com>
2025-02-23 10:25:09 +0200

    all

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,4 +1,44 @@
-const BlogCard = () => {
-  return <h2>BlogCard</h2>;
+import { formatDistanceToNow } from "date-fns";
+import styles from './BlogCard.module.css'
+import { formatDateNow } from "../../helpers/formatDataToNow";
+
+const BlogCard = ({
+  tag,
+  poster,
+  title,
+  descriprtion,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <div className={styles.card}>
+  <div className={styles.cardHeader}>
+    <img
+      className={styles.cardPoster}
+      src={poster}
+      alt={tag}
+    />
+  </div>
+  <div className={styles.cardBody}>
+    <span className={styles.tag}>{tag}</span>
+    <h2 className={styles.cardTitle}>What's new in 2022 Tech</h2>
+        <p className={styles.cardText}>{descriprtion}</p>
+  </div>
+  <div className={styles.cardFooter}>
+    <div className={styles.userBox}>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt={userName}
+      />
+      <div>
+        <h3 className={styles.userName}>{userName}</h3>
+            <small className={styles.date}>{formatDateNow(postedAt)}</small>
+      </div>
+    </div>
+  </div>
+</div>
+  );
 };
 export default BlogCard;
